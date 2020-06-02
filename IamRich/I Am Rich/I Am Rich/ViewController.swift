87e72ebe3e60b613fdc89708ac2dc7e98013f0b3
niroshansusl <niroshan.sub@gmail.com>
2020-06-03 02:32:05 +0800

    Adding IOS projects

---
+++ IamRich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Niroshan Rathnayake on 26/05/2020.
+//  Copyright © 2020 Niroshan Rathnayake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
